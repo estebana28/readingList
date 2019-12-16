409a3f58cc753a75ab0ddd93b98d9452a6540451
Esteban Arce <estebana@exo.com.ar>
2019-12-16 09:46:00 -0300

    Updates

---
+++ precache-manifest.00480b164442fe9c047b763c71e476dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d8230d3797cb6861c8e2b5028d0d7d",
+    "url": "/readingList/index.html"
+  },
+  {
+    "revision": "c5bdc813a845c61c6174",
+    "url": "/readingList/static/css/main.34ed3075.chunk.css"
+  },
+  {
+    "revision": "88484843ae0923300313",
+    "url": "/readingList/static/js/2.22a69789.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/readingList/static/js/2.22a69789.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5bdc813a845c61c6174",
+    "url": "/readingList/static/js/main.9e95d669.chunk.js"
+  },
+  {
+    "revision": "f1238765cce9796eeed7",
+    "url": "/readingList/static/js/runtime-main.73552f97.js"
+  }
+]);
